1b91d1ac07d50467935b434fb1875fb794491e01
Kenneth Long <kdlong@hep.wisc.edu>
2015-12-01 16:31:53 -0600

    Added genTaufilter

---
+++ DibosonGenAnalyzer/python/genTauFilter_cff.py
@@ -0,0 +1,24 @@
+import FWCore.ParameterSet.Config as cms
+
+hardProcessTausP = cms.EDFilter("GenParticleSelector",
+        src = cms.InputTag("prunedGenParticles"),
+        cut = cms.string("pdgId == 15 && isHardProcess()")
+)
+hardProcessTausM = cms.EDFilter("GenParticleSelector",
+        src = cms.InputTag("prunedGenParticles"),
+        cut = cms.string("pdgId == -15 && isHardProcess()")
+)
+hardProcessTaus = cms.EDProducer("CandViewMerger",
+    src = cms.VInputTag("hardProcessTausP", "hardProcessTausM")
+)
+tauFilter = cms.EDFilter("CandViewCountFilter",
+    src = cms.InputTag("hardProcessTaus"),
+    minNumber = cms.uint32(1)
+)
+
+filterGenTaus = cms.Sequence(
+    (hardProcessTausM + hardProcessTausP)*
+    hardProcessTaus*
+    tauFilter
+    #~tauFilter
+)
